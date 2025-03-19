89109a0dfb52b79b1ea187e210f997939cd070cb
lilianadj05 <lilianadj05@gmail.com>
2025-03-19 14:26:18 +0700

    Add files via upload

---
+++ iris_streamlit.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model = joblib.load('SVM.pickle')
+
+def main():
+    st.title('Machine Learning Model Deployment')
+
+    #add user input components for 5 features
+    sepal_length = st.slider('sepal_length', min_value=0.0, max_value=10.0, value=0.1)
+    sepal_width = st.slider('sepal_width', min_value=0.0, max_value=10.0, value=0.1)
+    petal_length = st.slider('petal_length', min_value=0.0, max_value=10.0, value=0.1)
+    petal_width = st.slider('petal_width', min_value=0.0, max_value=10.0, value=0.1)
+
+    if st.button('Make Prediction'):
+        features = [sepal_length, sepal_width, petal_length, petal_width]
+        result = make_prediction(features)
+        st.success(f'The prediction is{result}')
+
+
+def make_prediction(features):
+    input_array = np.array(features).reshape(1, -1)
+    prediction = model.predict(input_array)
+    return prediction[0]
+
+
+if __name__ == '__main__':
+    main()
